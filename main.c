9384515111c561e0652b3a57979fa90e44c88559
1337xp <79756837+1337xp@users.noreply.github.com>
2021-05-10 10:23:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ int main()
 {
     printf("Enter port value\n");
     scanf("%d", &a1);
-    printf("Enter port value\n");
+    printf("Enter RPM\n");
     scanf("%d", &a2);
-    printf("Enter port value\n");
+    printf("Enter line frequency (60/50) \n");
     scanf("%d", &a3);
     result = calculatevalue(a1,a2,a3);
     printf("%d\n", result);
